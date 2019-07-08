15290f6f435b3b2e19178c5c61e548d26df6807b
Alberto Cuadrado <Alberto.cuvi@gmail.com>
2019-07-08 18:13:03 +0200

    Created user controller class and added a new user

--- src/WebShopBundle/Controller/DefaultController.php
+++ src/WebShopBundle/Controller/DefaultController.php
@@ -4,14 +4,41 @@ namespace WebShopBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use ShopBundle\Entity\User;
+use Symfony\Component\HttpFoundation\Response;
 
-class DefaultController extends Controller
+/**
+ * @Route("/User")
+ */
+
+class UserController extends Controller
 {
-    /**
-     * @Route("/")
-     */
+
+    /*
     public function indexAction()
     {
         return $this->render('WebShopBundle/Default/index.html.twig');
+    }
+    */
+
+    /**
+     * @Route("/Add")
+     */
+
+    public function addUser()
+    {
+        $user = new User();
+        $user -> setName('Alberto');
+        $user -> setEmail('Alberto.cuvi@gmail.com');
+        $user -> setPassword('asdfg');
+
+        $user -> setCreateAt(new \DateTime('now'));
+        $user -> setUpdateAt(new \DateTime('now'));
+
+        $em = $this->getDoctrine()->getManager();
+        $em -> persist($user);
+        $em -> flush();
+
+        return new Response("Se ha generado un nuevo usuario" . $user->getId());
     }
 }
