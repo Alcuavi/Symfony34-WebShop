27f70852575a87d262547ddba0c78c51ac76b7ac
Alberto Cuadrado <Alberto.cuvi@gmail.com>
2019-07-09 12:19:26 +0200

    Column image added and generated images of products

--- src/WebShopBundle/Controller/ProductController.php
+++ src/WebShopBundle/Controller/ProductController.php
@@ -27,11 +27,12 @@ class ProductController extends Controller
     public function addProduct()
     {
         $product = new Product();
-        $product -> setModel('Casual2');
-        $product -> setBrand('Rayban2');
-        $product -> setPrice('99,30');
-        $product -> setQuantity('3');
+        $product -> setModel('Casual5');
+        $product -> setBrand('Rayban5');
+        $product -> setPrice('39,30');
+        $product -> setQuantity('5');
         $product -> setType('SunGlasses');
+        $product -> setImage('glass5.jpg');
 
         $product -> setCreateAt(new \DateTime('now'));
         $product -> setUpdateAt(new \DateTime('now'));
